21477c89495d97ba76ad701d709c98904273b552
Ronan Burke <burkuscat@gmail.com>
2023-10-07 00:00:20 +0100

    Demo app unit test project

--- samples/DemoApp/App.xaml.cs
+++ samples/DemoApp/App.xaml.cs
@@ -6,4 +6,9 @@ public partial class App : BurkusMvvmApplication
     {
         InitializeComponent();
     }
+
+#if NET7_0_OR_GREATER && !ANDROID && !MACCATALYST && !IOS && !WINDOWS
+    // workaround for unit tests in Maui https://github.com/dotnet/maui/issues/3552#issuecomment-1172606125
+    public static void Main(string[] args) {}
+#endif
 }
--- samples/DemoApp/ViewModels/DemoTabsViewModel.cs
+++ samples/DemoApp/ViewModels/DemoTabsViewModel.cs
@@ -1,5 +1,4 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.Input;
 using DemoApp.Views;
 
 namespace DemoApp.ViewModels;
