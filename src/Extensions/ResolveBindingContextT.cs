0a6897687aa9dee809351dddd405c24fe78d1535
Ronan Burke <burkuscat@gmail.com>
2023-10-13 19:24:56 +0100

    Fix ResolveBindingContext showing an error during development
    Fixes #13

---
+++ src/Extensions/ResolveBindingContextT.cs
@@ -0,0 +1,15 @@
+﻿namespace Burkus.Mvvm.Maui;
+
+public class ResolveBindingContext<T> : IMarkupExtension<T>
+    where T : class
+{
+    public object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return ServiceResolver.Resolve<T>();
+    }
+
+    T IMarkupExtension<T>.ProvideValue(IServiceProvider serviceProvider)
+    {
+        return ServiceResolver.Resolve<T>();
+    }
+}
